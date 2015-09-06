36fd795befcfc83b657317d9a07ebe8c7060b9ea
Cássio Marques <cassiommc@gmail.com>
2015-09-06 10:28:48 +0100

    Add documentation for ring middlewares.

--- src/memoria/handlers/app.clj
+++ src/memoria/handlers/app.clj
@@ -7,14 +7,23 @@
             [memoria.db :as db]
             [memoria.handlers.cards :as cards-handler]))
 
-(defn wrap-request-logging [app]
+(defn wrap-request-logging
+  "Logs the data received in the request and the data generated in the response. Useful
+  for debugging."
+  [app]
   (fn [request]
     (timbre/info (str "Received request: " request))
     (let [response (app request)]
       (timbre/info (str "Response: " response "\n"))
       response)))
 
-(defn wrap-db-conn [app]
+(defn wrap-db-conn
+  "Gets a new connection from the database connection pool and binds that to the
+  memoria.db/*conn* dynamic var.
+
+  If the `memoria-mode` header is present in the request and its value is `test`,
+  then the test connection pool will be used. Otherwise, the default pool is used."
+  [app]
   (fn [request]
     (let [datasource (if (= "test" (get-in request [:headers "memoria-mode"]))
                        (db/test-datasource)
