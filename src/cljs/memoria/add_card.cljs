fecf014f36747afa16c740410cd14c65b2b48644
Cássio Marques <cassiommc@gmail.com>
2015-09-10 22:56:24 +0100

    Reset form and atoms after submission

--- src/cljs/memoria/add_card.cljs
+++ src/cljs/memoria/add_card.cljs
@@ -5,38 +5,50 @@
             [memoria.data-binding :refer [bind-input]]
             [memoria.app :as app]))
 
-(defn- on-form-submit [event params]
+(def ^:private jquery (js* "$"))
+
+(def ^:private title-atom (r/atom nil))
+(def ^:private contents-atom (r/atom nil))
+(def ^:private tags-atom (r/atom nil))
+
+(defn- reset-inputs []
+  (reset! title-atom nil)
+  (reset! contents-atom nil)
+  (reset! tags-atom nil))
+
+(defn- on-form-submit [event]
   (.preventDefault event)
-  (do-post "/cards"
-           (fn [resp]
-             (app/load-latest-cards)
-             (modal/close-modal))
-           params))
+  (let [params {:title @title-atom
+                :contents @contents-atom
+                :tags @tags-atom}]
+    (do-post "/cards"
+             (fn [resp]
+               (app/load-latest-cards)
+               (reset-inputs)
+               (modal/close-modal))
+             params)))
 
 (defn add-card-modal []
-  (let [title (r/atom nil)
-        contents (r/atom nil)
-        tags (r/atom nil)]
+  [:div {:key "add-card-modal" :class "container memoria-modal"}
+   [:h2 {:class "ui aligned header"} "Add new card"]
+   [:div {:class "ui divider"}]
 
-    [:div {:key "add-card-modal" :class "container memoria-modal"}
-     [:h2 {:class "ui aligned header"} "Add new card"]
-     [:div {:class "ui divider"}]
-
-     [:form {:class "ui small form"
-             :action "#"
-             :on-submit #(on-form-submit %1 {:title @title
-                                             :contents @contents
-                                             :tags @tags})}
-      [:div {:class "required field"}
-       [:label "Title"]
-       [:input {:type "text" :on-change (bind-input title)}]]
-      [:div {:class "required field" :required true}
-       [:label "Content"]
-       [:textarea {:rows "5" :on-change (bind-input contents)}]]
-      [:div {:class "field"}
-       [:label "Tags"]
-       [:input {:type "text" :on-change (bind-input tags) :placeholder "tags are separated by spaces or commas"}]]
-      [:button {:class "ui center aligned button blue" :type "submit"} "Submit"]]]))
+   [:form {:class "ui small form"
+           :action "#"
+           :on-submit #(on-form-submit %1)}
+    [:div {:class "required field"}
+     [:label "Title"]
+     [:input {:value @title-atom :type "text" :on-change (bind-input title-atom)}]]
+    [:div {:class "required field" :required true}
+     [:label "Content"]
+     [:textarea {:value @contents-atom :rows "5" :on-change (bind-input contents-atom)}]]
+    [:div {:class "field"}
+     [:label "Tags"]
+     [:input {:type "text"
+              :value @tags-atom
+              :on-change (bind-input tags-atom)
+              :placeholder "tags are separated by spaces or commas"}]]
+    [:button {:class "ui center aligned button blue" :type "submit"} "Submit"]]])
 
 (defn add-card-button []
   (let [on-click (fn []
