100035f4ebedabfc1721bf7d2bbd5320dfb80840
Cássio Marques <cassiommc@gmail.com>
2015-09-20 12:51:01 -0300

    Adds soft-delete to the database layer

--- resources/sql/cards.sql
+++ resources/sql/cards.sql
@@ -1,19 +1,23 @@
 -- name: count-cards
 -- Counts the number of records existent in the cards table
-SELECT COUNT(*) FROM cards;
+SELECT COUNT(*) FROM cards WHERE deleted = 'f';
 
 -- name: select-latest-cards
 -- Selects latest cards
-SELECT id, title, tags, contents, created_at FROM cards WHERE id IN (SELECT DISTINCT ON (ancestor_id) id FROM cards ORDER BY ancestor_id DESC, id DESC LIMIT :limit OFFSET :offset) ORDER BY id DESC;
+SELECT id, title, tags, contents, created_at FROM cards WHERE id IN (SELECT DISTINCT ON (ancestor_id) id FROM cards ORDER BY ancestor_id DESC, id DESC LIMIT :limit OFFSET :offset) AND deleted = 'f' ORDER BY id DESC;
 
 -- name: search-cards
 -- Searches card using PostgreSQL full text search. Read http://shisaa.jp/postset/postgresql-full-text-search-part-3.html to
 -- better understand what it does.
-SELECT id, title, tags, contents, created_at, ts_rank(tsv, tsquery, 1), created_at as rank FROM cards, to_tsquery(:query) tsquery WHERE tsquery @@ tsv GROUP BY tsquery, id HAVING id IN (SELECT DISTINCT ON (ancestor_id) id FROM cards ORDER BY ancestor_id DESC, id DESC) ORDER BY rank DESC, id DESC LIMIT :limit OFFSET :offset;
+SELECT id, title, tags, contents, created_at, ts_rank(tsv, tsquery, 1), created_at as rank FROM cards, to_tsquery(:query) tsquery WHERE tsquery @@ tsv AND deleted = 'f' GROUP BY tsquery, id HAVING id IN (SELECT DISTINCT ON (ancestor_id) id FROM cards ORDER BY ancestor_id DESC, id DESC) ORDER BY rank DESC, id DESC LIMIT :limit OFFSET :offset;
 
 -- name: find-card-by-id
 -- Find a card by id
-SELECT id, title, tags, contents, ancestor_id, created_at FROM cards WHERE id = :id LIMIT 1;
+SELECT id, title, tags, contents, ancestor_id, created_at FROM cards WHERE id = :id AND deleted = 'f' LIMIT 1;
+
+-- name: find-deleted-card-by-id
+-- Find a deleted card by id
+SELECT id, title, tags, contents, ancestor_id, created_at FROM cards WHERE id = :id AND deleted = 't' LIMIT 1;
 
 -- name: insert-card<!
 -- Inserts a new record into the cards table
@@ -22,6 +26,10 @@ INSERT INTO cards (title, tags, contents, created_at) VALUES (:title, :tags, :co
 -- name: insert-card-with-ancestor<!
 -- Inserts a new record with an ancestor_id into the cards table
 INSERT INTO cards (title, tags, contents, ancestor_id, created_at) VALUES (:title, :tags, :contents, :ancestor_id, :created_at);
+
+-- name: soft-delete-card-by-id!
+-- Marks a card as soft deleted
+UPDATE cards SET deleted = 't' WHERE id = :id;
 
 -- name: delete-card-by-id!
 -- Deletes the card that has the given id
