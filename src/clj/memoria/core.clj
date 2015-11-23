55de7d4fc78c88f25482bb39dbaf3e17aa437d05
Cássio Marques <cassiommc@gmail.com>
2015-11-23 22:45:37 +0000

    Remove unused config namespace

--- src/clj/memoria/core.clj
+++ src/clj/memoria/core.clj
@@ -1,7 +1,5 @@
 (ns memoria.core
-  (:require [memoria.config :as config]
-            [memoria.handlers.app :as app-handlers]))
-
+  (:require [memoria.handlers.app :as app-handlers]))
 
 (def app app-handlers/app)
 
